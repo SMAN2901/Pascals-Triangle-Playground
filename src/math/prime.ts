d3f6c8b2225a892d4729e9d979640507ac9e3485
Sadman Rizwan <sadman.rizwan@gmail.com>
2025-02-24 08:15:09 +0600

    fixed prime filter

--- src/math/prime.ts
+++ src/math/prime.ts
@@ -3,6 +3,8 @@ import { NumberValidator } from './../models/NumberFilter';
 const isPrime: NumberValidator = (x: bigint): boolean => {
     if(x <= BigInt(3)) return x > BigInt(1);
 
+    if(x % BigInt(2) === BigInt(0)) return false;
+
     for(let i = BigInt(3); i * i <= x; i += BigInt(2)) {
         let y = x / i;
         if(y * i === x) return false;
